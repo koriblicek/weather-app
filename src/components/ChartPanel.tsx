f13b39d55b157a6d8e052b343e0a100b519ed0e9
koriblicek <koriblik@gmail.com>
2024-02-07 14:58:56 +0100

    small fix

--- src/components/ChartPanel.tsx
+++ src/components/ChartPanel.tsx
@@ -2,7 +2,7 @@ import { Alert, FormControl, InputLabel, MenuItem, Select, Stack } from '@mui/ma
 import { HourlyWeatherVariablesType, OpenMeteoDataStructureType } from 'src/types';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useState } from 'react';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
